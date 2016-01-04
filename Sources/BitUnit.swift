58579fa2fdce4e96cfaef817715f3489e0f870c9
Bastian Rinsche <bastian.rinsche@gmail.com>
2016-01-04 20:55:47 +0100

    Rename enums to full unit names

--- Sources/BitUnit.swift
+++ Sources/BitUnit.swift
@@ -1,30 +1,30 @@
 enum BitUnit: UInt {
     //TODO: Try to do this with 1024^x etc
     case Bit  = 1
-    case kbit = 1000
-    case Mbit = 1000000
-    case Gbit = 1000000000
-    case Tbit = 1000000000000
-    case Pbit = 1000000000000000
+    case Kilobit = 1000
+    case Megabit = 1000000
+    case Gigabit = 1000000000
+    case Terabit = 1000000000000
+    case Petabit = 1000000000000000
 
     case Byte  = 8
-    case kByte = 8000
-    case MByte = 8000000
-    case GByte = 8000000000
-    case TByte = 8000000000000
-    case PByte = 8000000000000000
+    case Kilobyte = 8000
+    case Megabyte = 8000000
+    case Gigabyte = 8000000000
+    case Terabyte = 8000000000000
+    case Petabyte = 8000000000000000
 
-    case KiBit = 1024
-    case MiBit = 1048576
-    case GiBit = 1073741824
-    case TiBit = 1099511627776
-    case PiBit = 1125899906842624
+    case Kibibit = 1024
+    case Mebibit = 1048576
+    case Gibibit = 1073741824
+    case Tebibit = 1099511627776
+    case Pebibit = 1125899906842624
 
-    case KiByte = 8192
-    case MiByte = 8388608
-    case GiByte = 8589934592
-    case TiByte = 8796093022208
-    case PiByte = 9007199254740992
+    case Kibibyte = 8192
+    case Mebibyte = 8388608
+    case Gibibyte = 8589934592
+    case Tebibyte = 8796093022208
+    case Pebibyte = 9007199254740992
 
     static func convert(count: UInt, from: BitUnit, to: BitUnit) -> UInt {
       if (from.rawValue < to.rawValue) {
